931e0a5137b4e1915b30c23b7d256fed04684186
Limon714 <limon4u714@gmail.com>
2022-08-31 15:28:11 +0600

    object

--- Reduce/reduce.js
+++ Reduce/reduce.js
@@ -1,4 +1,24 @@
-let arr = [20, 10];
-let sum = arr.reduce((prev, curr) => Math.pow(prev, curr) );
+const products = [
+    {
+        name: 'Pen',
+        amount: 5,
+        price:  50
+    },
+    
+    {
+        name: 'Paper',
+        amount: 2,
+        price:  100
+    },
+    
+    {
+        name: ' Book',
+        amount: 10,
+        price: 300 
+    }
+]
+let sum = products.reduce((prev, curr) => {
+    return prev + curr.amount * curr.price
+}, 20);
  
-console.log(sum);
+console.log(sum); 
